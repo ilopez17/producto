9b175a56be387252e664798a4c6626aeeb5df57d
iloez17 <isma1995chat@hotmail.com>
2019-11-15 18:55:39 -0600

    clase producto

---
+++ Producto/biblioteca/Producto.h
@@ -0,0 +1,16 @@
+#ifndef PRODUCTO_H
+#define PRODUCTO_H
+
+
+class Producto
+{
+    public:
+        Producto();
+        virtual ~Producto();
+
+    protected:
+
+    private:
+};
+
+#endif // PRODUCTO_H
