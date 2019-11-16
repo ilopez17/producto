d8b7176b8c21b3a4bc416f4f4b86e7fd877c7dc7
ilopez17 <57816833+ilopez17@users.noreply.github.com>
2019-11-15 18:19:52 -0600

    Add files via upload

---
+++ Producto.cpp
@@ -0,0 +1,66 @@
+#include "Producto.h"
+
+Producto::Producto(void)
+{
+
+
+
+}
+Producto::Producto(string N, int n, float p)
+{
+
+
+}
+void pideDatos(void)
+{
+
+
+
+}
+void muestraDatos(void)
+{
+
+
+}
+string dameNombre(void)
+{
+
+
+
+}
+void modificaNombre(string N)
+{
+
+
+
+}
+int dameNumero(void)
+{
+
+
+
+}
+void modificaNumero(int n)
+{
+
+
+
+}
+float damePrecio(void)
+{
+
+
+
+}
+void modificaPrecio(float p)
+{
+
+
+
+}
+void modificaDatos(string N, int n, float p)
+{
+
+
+
+}
